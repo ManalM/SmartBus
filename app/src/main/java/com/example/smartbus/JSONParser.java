59f343275be953d91f397268b1fbc7d0f4b33d00
ManalM <mmsaj000@hotmail.com>
2020-02-03 10:55:32 +0300

    SmartBus

---
+++ app/src/main/java/com/example/smartbus/JSONParser.java
@@ -0,0 +1,15 @@
+package com.example.smartbus;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import org.json.JSONObject;
+import android.util.Log;
+
+public class JSONParser {
+
+
+}
---
+++ app/src/main/java/com/example/smartbus/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.smartbus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText username,pass;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (EditText) findViewById(R.id.username);
+        pass = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+
+
+    }
+}
